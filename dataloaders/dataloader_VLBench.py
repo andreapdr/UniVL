b9eb4e505a1551ffa31e59a94405561a7379c5ac
andreapdr <andrea.pedrotti@isti.cnr.it>
2023-03-27 14:25:17 +0200

    manage pre-computed video features

--- dataloaders/dataloader_VLBench.py
+++ dataloaders/dataloader_VLBench.py
@@ -7,6 +7,7 @@ import sys
 
 sys.path.append(parent_dir)
 
+from math import ceil, floor
 from torch.utils.data import Dataset
 import torch
 import torch.nn.functional as F
@@ -28,6 +29,7 @@ class VLBenchDataset(Dataset):
         task="action",
         device="cpu",
         centercrop=True,
+        process_at_train=False,
     ):
         self._data = json.load(open(datapath))
         self.data = list(self._data.values())
@@ -41,6 +43,7 @@ class VLBenchDataset(Dataset):
         self.videodir = os.path.expanduser(videodir)
         self.device = device
         self.centercrop = centercrop
+        self.process_at_train = process_at_train
         print(f"- evaluating instrument: {self.instrument} on setting: {self.task}")
 
     def __len__(self):
@@ -69,29 +72,41 @@ class VLBenchDataset(Dataset):
         }
 
     def _get_video(self, video_id):
-        video_feat = self._extract_features(video_id)
-        video_mask = torch.ones(size=(1, video_feat.shape[0]))
+        if self.process_at_train:
+            video_feat = self._extract_features(video_id)
+        else:
+            if self.data[video_id]["youtube_id"] is not None:
+                video_fname = self.data[video_id]["youtube_id"]
+            else:
+                video_fname = self.data[video_id]["video_file"]
+            feature_path = os.path.join("tmp", "processed", video_fname + ".npy")
+            video_feat = np.load(feature_path)
+        video_mask = torch.ones(size=(1, video_feat.shape[0]))  # TODO: check this
         return {"video_feat": video_feat, "video_mask": video_mask}
 
     def __getitem__(self, idx):
         text = self._get_text(idx)
         video = self._get_video(idx)
-        return video, text
+        video_id = self.data[idx]["top_level_key"]
+        return video, text, video_id
 
     def _extract_features(self, idx):
         if self.data[idx]["youtube_id"] is not None:
             video_fname = self.data[idx]["youtube_id"] + ".mp4"
         else:
-            video_fname = self.data[idx][
-                "video_file"
-            ]  # TODO: what about the extension?
+            video_fname = self.data[idx]["video_file"]
+            if self.data[idx]["dataset"] == "smsm":
+                video_fname += ".webm"
+            elif self.data[idx]["dataset"] == "ikea":
+                video_fname += ".avi"
+            else:
+                video_fname += ".mp4"
+
         video_path = os.path.join(self.videodir, video_fname)
-        video = torchvision.io.read_video(
-            video_path,
-            pts_unit="sec",
-            start_pts=self.data[idx]["timestamp"][0],
-            end_pts=self.data[idx]["timestamp"][1],
-        )[0].permute(0, 3, 1, 2)
+        start_time = self.data[idx]["start_time"]
+        end_time = self.data[idx]["end_time"]
+
+        video = self._load_video(video_path, start_time, end_time)
 
         if self.centercrop:
             video = torchvision.transforms.CenterCrop((224, 224))(video)
@@ -99,9 +114,23 @@ class VLBenchDataset(Dataset):
         with torch.no_grad():
             self.video_feat_extractor.eval()
             video = video.squeeze()
+            # Batch x 3 x T x H x W
             video = self.video_preprocessor(video)
-            video_batch = video.to(self.device)
-            batch_features = self.video_feat_extractor(video_batch)
-            batch_features = F.normalize(batch_features, dim=1)
-            features = batch_features.cpu().numpy()
-        return features
+            video = video.to(self.device)
+            video = self.video_feat_extractor(video)
+            video = F.normalize(video, dim=1)
+            # video = video.cpu().numpy()
+        return video
+
+    def _load_video(Self, video_path, start_time=None, end_time=None):
+        if start_time is None:
+            return torchvision.io.read_video(video_path, pts_unit="sec")[0].permute(
+                0, 3, 1, 2
+            )
+        else:
+            return torchvision.io.read_video(
+                video_path,
+                pts_unit="sec",
+                start_pts=floor(start_time),
+                end_pts=ceil(end_time),
+            )[0].permute(0, 3, 1, 2)
